92a6787b700cecbafa5edaefc6c993f7a388285d
unknown <rockmanas121@gmail.com>
2024-01-21 19:36:26 +0530

    Add more button

--- script.js
+++ script.js
@@ -1,17 +1,21 @@
-if( $(window).width() < 948){
-    $('.navbar .menu').toggle();
+if ($(window).width() < 948) {
+  $(".navbar .menu").toggle();
 }
 
-$(document).ready(function(){
-    $(window).scroll(function(){
-        if(this.scrollY > 20){
-            $('.navbar').addClass("sticky");
-        } else {
-            $('.navbar').removeClass("sticky");
-        }
-    });
+$(document).ready(function () {
+  $(window).scroll(function () {
+    if (this.scrollY > 20) {
+      $(".navbar").addClass("sticky");
+    } else {
+      $(".navbar").removeClass("sticky");
+    }
+  });
 
-    $('.menu-btn').click(function(){
-        $('.navbar .menu').toggle("active");
-    })
-})
+  $(".menu-btn").click(function () {
+    $(".navbar .menu").toggle("active");
+  });
+
+  $(".menu").click(function () {
+    $(".navbar .menu").toggle("active");
+  });
+});
